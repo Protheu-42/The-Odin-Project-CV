d7d00694a6a9bbc35d28f03ec4f29c42addc976d
Protheu <everaldo.sobrinho42@gmail.com>
2026-05-15 18:53:59 -0300

    feat: create form and read mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import EducationalFieldInfo from "./components/EducationalFieldInfo";
 import PracticalExperience from "./components/PracticalExperience";
 
 function App() {
+  const [visualizationMode, setVisualizationMode] = useState("form");
+
   const [generalInfo, setGeneralInfo] = useState({
     name: "",
     email: "",
@@ -97,43 +99,80 @@ function App() {
       }));
     }
   }
-  return (
-    <div>
-      <h1>Input your CV</h1>
-      <form>
-        <fieldset>
-          <legend>General Information</legend>
-          <GeneralInfoField
-            name={generalInfo.name}
-            email={generalInfo.email}
-            phoneNumber={generalInfo.phoneNumber}
-            onChangeHandler={handleGeneralChange}
-          />
-        </fieldset>
-        <fieldset>
-          <legend>Educational experience</legend>
-          <EducationalFieldInfo
-            schoolName={educationalInfo.schoolName}
-            titleOfStudy={educationalInfo.titleOfStudy}
-            dateOfStart={educationalInfo.dateOfStart}
-            dateOfEnding={educationalInfo.dateOfEnding}
-            onChangeHandler={handleEducationalChange}
-          />
-        </fieldset>
-        <fieldset>
-          <legend>Practical Experience</legend>
-          <PracticalExperience
-            companyName={experienceInfo.companyName}
-            positionTitle={experienceInfo.positionTitle}
-            mainResponsibilities={experienceInfo.mainResponsibilities}
-            workStarted={experienceInfo.workStarted}
-            workEnded={experienceInfo.workEnded}
-            onChangeHandler={handlePracticalExperience}
-          />
-        </fieldset>
-      </form>
-    </div>
-  );
+  if (visualizationMode === "form") {
+    return (
+      <div>
+        <h1>Input your CV</h1>
+        <form>
+          <fieldset>
+            <legend>General Information</legend>
+            <GeneralInfoField
+              name={generalInfo.name}
+              email={generalInfo.email}
+              phoneNumber={generalInfo.phoneNumber}
+              onChangeHandler={handleGeneralChange}
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Educational experience</legend>
+            <EducationalFieldInfo
+              schoolName={educationalInfo.schoolName}
+              titleOfStudy={educationalInfo.titleOfStudy}
+              dateOfStart={educationalInfo.dateOfStart}
+              dateOfEnding={educationalInfo.dateOfEnding}
+              onChangeHandler={handleEducationalChange}
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Practical Experience</legend>
+            <PracticalExperience
+              companyName={experienceInfo.companyName}
+              positionTitle={experienceInfo.positionTitle}
+              mainResponsibilities={experienceInfo.mainResponsibilities}
+              workStarted={experienceInfo.workStarted}
+              workEnded={experienceInfo.workEnded}
+              onChangeHandler={handlePracticalExperience}
+            />
+          </fieldset>
+          <button onClick={() => setVisualizationMode("read")}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+
+  if (visualizationMode === "read") {
+    return (
+      <div>
+        <h1>{generalInfo.name}</h1>
+
+        <h2>Educational History</h2>
+        <h3>{educationalInfo.schoolName}</h3>
+        <p>Title of Study: {educationalInfo.titleOfStudy}</p>
+        <p>
+          Start at {educationalInfo.dateOfStart} until{" "}
+          {educationalInfo.dateOfEnding}
+        </p>
+
+        <h2>Career History</h2>
+        <h3>{experienceInfo.companyName}</h3>
+        <p>Position Title: {experienceInfo.positionTitle}</p>
+        <p>
+          My main responsibilities were {experienceInfo.mainResponsibilities}
+        </p>
+        <p>
+          Work there from {experienceInfo.dateOfStart} to{" "}
+          {experienceInfo.dateOfEnding}
+        </p>
+        <h3>Contact</h3>
+        <ul>
+          <li>Phone Number: {generalInfo.phoneNumber}</li>
+          <li>email: {generalInfo.email}</li>
+        </ul>
+
+        <button onClick={() => setVisualizationMode("form")}>Edit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
